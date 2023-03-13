0c42be223d69d2dabbb75844abfc9996707cb794
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-03-13 11:45:08 -0400

    this is a maintenance push

---
+++ src/_data/pokeDetails.js
@@ -0,0 +1,16 @@
+// const pokeDetailsFetch = require("@11ty/eleventy-fetch");
+// const url = `https://pokeapi.co/api/v2/pokemon/`;
+
+
+// async function getPokeDetails() {
+//   const response = pokeDetailsFetch(url, {
+//     duration: "1h",
+//     type: "json",
+//     directory: "./src/_data/pokeDetails.json",
+//   });
+//   const pokeData = await response;
+//   return pokeData;
+// }
+
+
+// module.exports = getPokeDetails;
